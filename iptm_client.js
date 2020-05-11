29b6d8faf6c6024487a1ded97647e2638c578853
iExploTech <iexplotech@gmail.com>
2020-05-11 16:52:47 +0800

    update host: 'bootnodes.iptm.online'

--- iptm_client.js
+++ iptm_client.js
@@ -330,7 +330,7 @@ function connectToServer () {
 	// Options to be used by for http request 
 	var options = {
 	//	host: 'localhost',
-		host: 'iptm.online',
+		host: 'bootnodes.iptm.online',
 		port: '8080',
 		path: '/list_bootnodes.json',
 	//	path: '/table.html',
